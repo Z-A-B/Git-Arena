6c47f960a1ec4e8fa2cde87b711a1033e4a9fc3e
Antonin Do Souto <avainfoconseils@gmail.com>
2025-11-03 11:26:46 +0000

    feat: formatted app.js + event started

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const log = document.getElementById('log');
-  log.textContent = [
-    'v0.0.1: Repo initialisé.',
-    'Consigne: créer deux branches et provoquer un petit conflit contrôlé.',
-    'Astuce: modifiez la même ligne du <h1> ci-dessus pour déclencher un conflit.'
-  ].join('\n');
+    const log = document.getElementById('log');
+    log.textContent = [
+        'v0.0.1: Repo initialisé.',
+        'Consigne: créer deux branches et provoquer un petit conflit contrôlé.',
+        'Astuce: modifiez la même ligne du <h1> ci-dessus pour déclencher un conflit.'
+    ].join('\n');
 });
+
+console.log("Project Started")
